56fa46407b81dfa882cbc518692384effb65626c
yujinHeo <youjin2586@gmail.com>
2025-03-20 11:26:44 +0900

    commit

--- lightbooks/src/main/java/com/itwill/lightbooks/repository/novel/NovelRepository.java
+++ lightbooks/src/main/java/com/itwill/lightbooks/repository/novel/NovelRepository.java
@@ -1,7 +1,5 @@
 package com.itwill.lightbooks.repository.novel;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +7,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.itwill.lightbooks.domain.Novel;
-import com.itwill.lightbooks.domain.NovelRating;
 
 public interface NovelRepository extends JpaRepository<Novel, Long>, NovelQuerydsl {
 	
