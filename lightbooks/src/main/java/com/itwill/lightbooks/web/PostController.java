ea2ab0500a5585f49d4f8bd79c3e4158e8fcbacf
Jihae Lee <wlgo0068@naver.com>
2025-03-24 18:54:47 +0900

    header modify and notice

---
+++ lightbooks/src/main/java/com/itwill/lightbooks/web/PostController.java
@@ -0,0 +1,58 @@
+package com.itwill.lightbooks.web;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.itwill.lightbooks.domain.Post;
+import com.itwill.lightbooks.service.PostService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/post")
+public class PostController {
+	
+	private final PostService postService;
+
+	@GetMapping("/notice")
+	public void readNotices() {
+		log.info("readNotices");
+	}
+	
+	@GetMapping("/notice/details")
+	public String detailsNotice(Model model, @RequestParam(name = "id") Long id) {
+		log.info("detailsNotice(id={})");
+		
+		Post post = postService.read(id);
+		
+		model.addAttribute("post", post);
+		
+		return "/post/details";
+	}
+	
+	@ResponseBody
+	@GetMapping("/notice/read")
+	public ResponseEntity<List<Post>> readNotice() {
+		List<Post> posts = postService.readAll();
+		
+		return ResponseEntity.ok(posts);
+	}
+	
+	@ResponseBody
+	@GetMapping("/notice/read/highlight")
+	public ResponseEntity<List<Post>> readNoticeHighlight() {
+		List<Post> posts = postService.readOnlyHighlight();
+		
+		return ResponseEntity.ok(posts);
+	}
+}
